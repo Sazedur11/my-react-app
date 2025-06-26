243554075213db6257c18b6223ab847a73e2e940
Sazedur <sazedur11ab@gmail.com>
2025-06-26 23:05:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Todo from "./Todo";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Todo task="Learn React"></Todo>
+      <Todo task="Learn React core concept"></Todo>
+      <Todo task="Repeat react js again"></Todo>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Todo.jsx
@@ -0,0 +1,5 @@
+export default function Todo({task}){
+    return(
+        <h2>New Task: {task}</h2>
+    )
+}
